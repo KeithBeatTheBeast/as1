3e2e1383c34999f98fb83f7ada243344d911cb87
KeithBeatTheBeast <keithgmills34@gmail.com>
2016-09-29 23:02:15 -0600

    Gson running for first activity

--- app/src/main/java/com/example/keith/kgmills_habittracker/habitsViewActivity.java
+++ app/src/main/java/com/example/keith/kgmills_habittracker/habitsViewActivity.java
@@ -9,6 +9,19 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
 /** To do list according to notes:
     For the random strings held here and there, a string holder class analogous to strings.xml
     Strings.xml, do it
@@ -20,25 +33,23 @@ public class habitsViewActivity extends AppCompatActivity {
 
     private static final String HABITFILE = "habitFile.sav";
     private ListView activeHabitsListView;
-    private HabitController hCtl = new HabitController();
+    private HabitController hCtl;
     private ArrayAdapter<Habit> adapter;
 
+
     // REMOVE LATER ONLY FOR TESTING/DESIGNING
-    private static final Boolean[] daysInAWeek = {false, true, true, true, true, true, false};
+    private static final Boolean[] daysInAWeek = {true, true, true, true, true, true, true};
     private String name = "aNewName";
     private static final String testDate = "1995-01-25";
     private Habit testHabit = new Habit(name, testDate, daysInAWeek);
     private Habit anotherTestHabit = new Habit("Another", testDate, daysInAWeek);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habits_view);
         activeHabitsListView = (ListView) findViewById(R.id.activeHabitsListView);
-        hCtl.addAHabit(testHabit);
-        hCtl.addAHabit(anotherTestHabit);
-        adapter = new ArrayAdapter<Habit>(this, R.layout.list_item, hCtl.getActiveHabits());
-        activeHabitsListView.setAdapter(adapter);
 
         // http://stackoverflow.com/questions/21295328/android-listview-with-onclick-items
         activeHabitsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -46,6 +57,7 @@ public class habitsViewActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?>adapt, View v, int position, long l) {
                 hCtl.getActiveHabits().get(position).completion();
+                saveInFile();
                 adapter.notifyDataSetChanged();
             }
         });
@@ -60,6 +72,53 @@ public class habitsViewActivity extends AppCompatActivity {
 
         });
 
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        loadFromFile();
+        hCtl.addAHabit(testHabit);
+        hCtl.addAHabit(anotherTestHabit);
+        adapter = new ArrayAdapter<Habit>(this, R.layout.list_item, hCtl.getActiveHabits());
+        adapter.notifyDataSetChanged();
+        activeHabitsListView.setAdapter(adapter);
+    }
+
+    private void loadFromFile() {
+        try {
+            FileInputStream fis = openFileInput(HABITFILE);
+            BufferedReader in = new BufferedReader(new InputStreamReader(fis));
+
+            Gson gson = new Gson();
+
+            // From StackOverFlow (GET ORIGINAL THREAD)
+            Type listtype = new TypeToken<HabitController>() {
+            }.getType();
+
+            hCtl = gson.fromJson(in, listtype);
+        } catch (FileNotFoundException e) {
+            hCtl = new HabitController();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
+    }
+
+    private void saveInFile() {
+        try {
+            FileOutputStream fos = openFileOutput(HABITFILE, 0);
+            BufferedWriter out = new BufferedWriter(new OutputStreamWriter(fos));
+
+            Gson gson = new Gson();
+            gson.toJson(hCtl, out);
+            out.flush();
+
+            fos.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException();
+        } catch (IOException e) {
+            throw new RuntimeException();
+        }
     }
 
     public void addNewHabitIntent(View view) {
